a6151b4f86701bc7893a454bc2b8030244887873
lance butler <lancej2004@gmail.com>
2024-03-21 15:00:18 -0600

    Test: US 6, orders most recent to least recent method

--- spec/models/f1_team_spec.rb
+++ spec/models/f1_team_spec.rb
@@ -10,4 +10,19 @@ RSpec.describe F1Team, type: :model do
     it {should validate_presence_of :horsepower}
     #it {should validate_presence_of :slick_tires}
   end
+
+  describe "class methods" do
+    describe "self.recent_team" do
+      before do
+        @mercedes = F1Team.create!(name: "Mercedes", horsepower: 1000, slick_tires: true, created_at: "2022-04-22")
+        @haas = F1Team.create!(name: "Haas", horsepower: 1000, slick_tires: true, created_at: "2023-09-13")
+        @red_bull =  F1Team.create!(name: "Red Bull", horsepower: 1002, slick_tires: true, created_at: "2024-03-21")
+
+      end
+
+      it "orders most recent to least recent" do
+        expect(F1Team.recent_team).to match([@red_bull, @haas, @mercedes])
+      end
+    end
+  end
 end
