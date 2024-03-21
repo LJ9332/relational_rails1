c0882ed85cb705683b9ecc72e8182ff027ba4db8
lance butler <lancej2004@gmail.com>
2024-03-20 22:47:03 -0600

    Test: vaildations

--- spec/models/f1_driver_spec.rb
+++ spec/models/f1_driver_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe F1Driver, type: :model do
   describe 'relationships' do
     it {should belong_to(:f1_team)}
   end
+
+  describe 'vaildations' do
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :weight}
+    #it {should validate_presence_of :world_champion}
+  end
 end
--- spec/models/f1_team_spec.rb
+++ spec/models/f1_team_spec.rb
@@ -2,6 +2,12 @@ require "rails_helper"
 
 RSpec.describe F1Team, type: :model do
   describe 'relationships' do
-   it {should have_many(:f1_drivers)}
+    it {should have_many(:f1_drivers)}
+  end
+
+  describe 'validations' do
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :horsepower}
+    #it {should validate_presence_of :slick_tires}
   end
 end
