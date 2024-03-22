219b214387577496155f612267535853eff8dfd8
lance butler <lancej2004@gmail.com>
2024-03-21 18:48:54 -0600

    Test: US 7, counts the amount of drivers for a single team

--- spec/features/f1_teams/show_spec.rb
+++ spec/features/f1_teams/show_spec.rb
@@ -4,6 +4,9 @@ RSpec.describe "F1 teams show page" do
   before(:each) do
     @mercedes = F1Team.create!(name: "Mercedes", horsepower: 1001, slick_tires: true)
     @haas = F1Team.create!(name: "Haas", horsepower: 1000, slick_tires: false)
+
+    @luis = @mercedes.f1_drivers.create!(name: "Luis Hamilton", weight: 163, world_champion: true)
+    @valtteri = @mercedes.f1_drivers.create!(name: "Valtteri Bottas", weight: 158, world_champion: false)
   end
   
   
@@ -18,6 +21,15 @@ RSpec.describe "F1 teams show page" do
       expect(page).to_not have_content(@haas.name)
       expect(page).to_not have_content(@haas.horsepower)
       expect(page).to_not have_content(@haas.slick_tires)
+    end
+  end
+
+  describe 'User Story 7' do
+    it "counts the amount of drivers for a single team" do
+      visit "f1_teams/#{@mercedes.id}"
+
+      expect(page).to have_content(@mercedes.name)
+      expect(page).to have_content("Number of Drivers: 2")
     end
   end
 end
