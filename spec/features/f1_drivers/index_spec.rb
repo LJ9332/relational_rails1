e9cee85573e9f8a363887cb83fad6bb8a7fbf3bc
lance butler <lancej2004@gmail.com>
2024-03-25 13:57:53 -0600

    Test: US 7 & 8, Displays link to f1 drivers and teams index on all pages

--- spec/features/f1_drivers/index_spec.rb
+++ spec/features/f1_drivers/index_spec.rb
@@ -22,4 +22,40 @@ RSpec.describe "F1 Drivers index page" do
       expect(page).to have_content(@kevin.world_champion)
     end
   end
+
+  describe 'User Story 7' do
+    it "Displays link to f1 drivers index on all pages" do
+      visit "/f1_teams"
+
+      expect(page).to have_link("F1 Drivers", href: "/f1_drivers")
+      click_on "F1 Drivers"
+      expect(current_path).to eq("/f1_drivers")
+    end
+
+    it "Displays link to f1 drivers index on all pages" do
+      visit "/f1_drivers"
+
+      expect(page).to have_link("F1 Drivers", href: "/f1_drivers")
+      click_on "F1 Drivers"
+      expect(current_path).to eq("/f1_drivers")
+    end
+  end
+
+  describe 'User Story 8' do
+    it "Displays link to f1 teams index on all pages" do
+      visit "/f1_drivers"
+
+      expect(page).to have_link("F1 Teams")
+      click_on "F1 Teams"
+      expect(current_path).to eq("/f1_teams")
+    end
+
+    it "Displays link to f1 teams index on all pages" do
+      visit "/f1_teams"
+
+      expect(page).to have_link("F1 Teams")
+      click_on "F1 Teams"
+      expect(current_path).to eq("/f1_teams")
+    end
+  end
 end
