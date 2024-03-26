2c8ca110aa8781b0e30d0012fb72e68e5a00db74
lance butler <lancej2004@gmail.com>
2024-03-26 16:43:31 -0600

    Test: US 13, Displays a link to create a F1 Driver

--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,5 @@ Rails.application.routes.draw do
 
   get "/f1_teams/:f1_team_id/f1_drivers", to: "f1_team_drivers#index"
   get "/f1_teams/:f1_team_id/f1_drivers/new", to: "f1_team_drivers#new"
+  post "/f1_teams/:f1_team_id/f1_drivers", to: "f1_team_drivers#create"
 end
