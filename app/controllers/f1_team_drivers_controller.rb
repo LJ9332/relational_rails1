d8402522c6d09dbee65e958875cfcacdff7f987d
lance butler <lancej2004@gmail.com>
2024-03-26 16:46:00 -0600

    Feat: US 13, Displays a link to create a F1 Driver

--- app/controllers/f1_team_drivers_controller.rb
+++ app/controllers/f1_team_drivers_controller.rb
@@ -3,4 +3,19 @@ class F1TeamDriversController < ApplicationController
     @f1_team = F1Team.find(params[:f1_team_id])
     @f1_drivers = @f1_team.f1_drivers
   end
+
+  def new
+    @f1_team = F1Team.find(params[:f1_team_id])
+  end
+  
+  def create
+    @f1_team = F1Team.find(params[:f1_team_id])
+    @f1_team.f1_drivers.create!({
+      name: params[:name],
+      weight: params[:weight],
+      world_champion: params[:world_champion]
+    })
+    
+    redirect_to "/f1_teams/#{@f1_team.id}/f1_drivers"
+  end
 end
